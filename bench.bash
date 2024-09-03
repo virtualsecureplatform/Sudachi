9fa5d6b1189edc2f831c02967ec11ca0c14d58f0
nindanaoto <matsuoka.kotaro@gmail.com>
2024-09-03 19:28:05 +0000

    I hope it will fix the issue

--- bench.bash
+++ bench.bash
@@ -4,7 +4,9 @@ DATE=$(date +'%Y%m%d%H%M%S')
 mkdir -p log/$DATE
 cmake . -G Ninja -B /tmp/build -DENABLE_TEST=ON  -DUSE_AVX512=ON -DUSE_CONCRETE=ON
 cd /tmp/build
+# run twice to make sure to avoid the sbt conflict
 ninja
+ninja 
 cd test/iscas85
 for circuit in c17 c432 c499 c880 c1355 c1908 c2670 c3540 c5315 c6288 c7552; do
     cd $circuit
